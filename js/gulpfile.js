299d232f543ac832487e9cc08369dcfc3de77cd7
miriam.aumatell <m.aumatell@scrm.eu>
2021-10-24 13:32:11 +0200

    delete unused js files and modify path in gulpfile.js

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -3,13 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function () {
-    return gulp.src('./flexbin.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
+gulp.task('default', function() {
+    return gulp.src('./css/flexbin.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'));
 });
 
 // Watch SASS file changes
-gulp.task('watch', function () {
-    gulp.watch('./flexbin.scss', ['default']);
+gulp.task('watch', function() {
+    gulp.watch('./css/flexbin.scss', ['default']);
 });
